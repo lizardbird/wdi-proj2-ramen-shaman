e4857f99bca808d495f959cfc1ef348910ca725b
Liz Jewell <eghjewell@gmail.com>
2017-08-06 19:04:30 -0400

    review MCV complete

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,55 @@
+class ReviewsController < ApplicationController
+
+  # GET /reviews
+  def index
+    @reviews = Review.all
+  end
+
+  # GET /reviews/new
+  def new
+    @review = Review.new
+  end
+
+  # POST /reviews
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      flash[:notice] = "#{@review.title} created successfully."
+      redirect_to review_path(@review)
+    else
+      render :new
+    end
+  end
+
+  # GET /reviews/1
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  # GET /reviews/1/edit
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  # PATCH/PUT /reviews/1
+  def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    flash[:notice] = "Review updated successfully."
+    redirect_to review_path(@review)
+  end
+
+  # DELETE /reviews/1
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:alert] = "Review deleted!"
+    redirect_to reviews_path
+  end
+
+  private
+    # Prevent evil people from messing with my params
+    def review_params
+      params.require(:review).permit(:title, :content, :img_url)
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Review.destroy_all
+
+review1 = Review.create(
+  {
+    title: "This is a review",
+    content: "Butter Nissin instant cup ramen ramen burger Nissin instant cup ramen Tokushima soy sauce scallions abura soba yuzu bamboo slices pork bones bamboo slices rice Yokohama soy sauce toasted sesame seeds Tokyo rice. Fish broth miso abura soba pork bones soy sauce Hakata corn Hakodate scallions curry Asahikawa soy sauce pork cubes. Hakata bean sprouts pork bones miso chilli flavoured oil minced garlic Kagoshima. Hakata fish broth Asahikawa pork cubes Wakayama Kagoshima leek Kagoshima lard spinach Tokyo.",
+    img_url: "https://ramenipsum.herokuapp.com/assets/img/afuri_yuzu_shio.jpg"
+  }
+)
