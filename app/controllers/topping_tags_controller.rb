815f2c4c514b4b77e2562aa29ac4a870c19cf5e6
Liz Jewell <eghjewell@gmail.com>
2017-08-09 09:25:15 -0400

    testing adding tags within bowls form

--- app/controllers/topping_tags_controller.rb
+++ app/controllers/topping_tags_controller.rb
@@ -6,17 +6,26 @@ class ToppingTagsController < ApplicationController
 
   def create
     @bowl = Bowl.find(params[:bowl_id])
-    @topping = Topping.find_by(topping_params)
-    if ToppingTag.find_by(bowl: @bowl, topping: @topping)
-    else
-      @topping_tag = @bowl.topping_tags.create
-      if @topping_tag.topping = @topping
-      else
-        @topping_tag.topping = Topping.create(topping_params)
-      end
-      @topping_tag.save
+    @topping = Topping.find_or_create_by(name: params[:topping_name])
+    # @topping = Topping.find_by(topping_params)
+    existing_tag = ToppingTag.find_by(bowl: @bowl, topping: @topping)
+    unless existing_tag
+      @bowl.topping_tags.create(topping: @topping)
     end
     redirect_to bowl_path(@bowl)
+  end
+    #
+    #
+    # if ToppingTag.find_by(bowl: @bowl, topping: @topping)
+    # else
+    #   @topping_tag = @bowl.topping_tags.create
+    #   if @topping_tag.topping = @topping
+    #   else
+    #     @topping_tag.topping = Topping.create(topping_params)
+    #   end
+    #   @topping_tag.save
+    # end
+    # redirect_to bowl_path(@bowl)
   end
 
   private
