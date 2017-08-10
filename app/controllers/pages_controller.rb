3bc3c774531eceee9391b145c8322c667d200787
Liz Jewell <eghjewell@gmail.com>
2017-08-10 10:12:12 -0400

    all the CSS

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,15 +4,15 @@ class PagesController < ApplicationController
   def myprofile
   end
   def show
-      if valid_page?
-        render template: "pages/#{params[:page]}"
-      else
-        render file: "public/404.html", status: :not_found
-      end
-    end
-
-    private
-    def valid_page?
-      File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
+    if valid_page?
+      render template: "pages/#{params[:page]}"
+    else
+      render file: "public/404.html", status: :not_found
     end
   end
+
+  private
+  def valid_page?
+    File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
+  end
+end
